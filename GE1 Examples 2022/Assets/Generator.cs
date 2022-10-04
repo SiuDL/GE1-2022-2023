b1db78a61a8c9de49d8ffcd9f6033d5b4ad34788
SiuDL <siudzoen@gmail.com>
2022-10-05 00:34:45 +0100

    red decha

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -2,30 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generator : MonoBehaviour
+public class Generator : MonoBehaviour 
 {
-    public int loops = 10;
-    public float angle;
-    public float a = 16f;
-    public float n = 10f;
+    int loops = 20;
+    int n = 0;
+    float r,c;
     public GameObject prefab;
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        for(int i=0;i<loops;i++)
+        while(r >= 0) 
         {
-            for(int j=0;j<n;++j)
+            loops--;
+            r = loops/2;
+            c = 2*Mathf.PI*r;
+            n = (int)(c/(2/2));
+            float side = 2*r*Mathf.Sin(Mathf.PI/n);
+
+            while(n>1 && side<2/2) 
             {
-                angle = Mathf.PI * 2f * (j / n);
-                GameObject.Instantiate(prefab, new Vector3(a*Mathf.Cos(angle), a*Mathf.Sin(angle), 0f), Quaternion.identity);
+                n = n - 1;
             }
-            a -= 2.0f;
+
+            for (int i=0;i<n;++i)
+            {
+                float angle = Mathf.PI*2f*i/n;
+                GameObject.Instantiate(prefab, new Vector3(r*Mathf.Cos(angle), r*Mathf.Sin(angle), 0f), Quaternion.identity);
+            }
+            r = r - 1;
         }
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
