e240f3c1ce25e87960cec1afa41fa9535684dd48
SiuDL <siudzoen@gmail.com>
2022-10-04 21:43:31 +0100

    update

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -6,20 +6,20 @@ public class Generator : MonoBehaviour
 {
     public int loops = 10;
     public float angle;
-    public float a = 2f;
-    public float n = 4;
+    public float a = 16f;
+    public float n = 20;
     public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
         for(int i=0;i<loops;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<n;++j)
             {
                 angle = Mathf.PI * 2f * j / n;
                 GameObject.Instantiate(prefab, new Vector3(a*Mathf.Cos(angle), a*Mathf.Sin(angle), 0f), Quaternion.identity);
             }
-            a -= 2*2f;
+            a -= 2*(2f);
         }
     }
 
