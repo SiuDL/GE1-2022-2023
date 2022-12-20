4ad7f7da9b444aaced97b1d25670a567dfb74bb6
SiuDL <siudzoen@gmail.com>
2022-12-20 02:44:37 +0000

    button animation/interaction - clean up (stable version)

--- GE1 Assignment/Assets/Scripts/Interactables/ButtonManager.cs
+++ GE1 Assignment/Assets/Scripts/Interactables/ButtonManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : Interactable
+public class ButtonManager : Interactable
 {
     [SerializeField]
     private GameObject button;
