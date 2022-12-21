0096dce3c3ad4a058f4291a2eef1e76828b72dc7
SiuDL <siudzoen@gmail.com>
2022-12-21 01:29:02 +0000

    robot switcher - completed (stable version)

--- GE1 Assignment/Assets/Scripts/Switcher/RobotSwitcher.cs
+++ GE1 Assignment/Assets/Scripts/Switcher/RobotSwitcher.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class RobotSwitcher: MonoBehaviour
 {
+    // serialized field to store the robot prefabs
     [SerializeField]
+    private GameObject robotPrefab1;
+    [SerializeField]
+    private GameObject robotPrefab2;
+
+    // variables to contain the clones of the prefabs
     private GameObject robot1;
-    [SerializeField]
     private GameObject robot2;
+
+    // boolean to check if robot has been switched
     private bool switched = false;
 
+    // method to change the robot
     public void ChangeRobot()
     {
         switch(switched)
         {
             case false:
-                Instantiate(robot2, transform.position, transform.rotation);
+                if(robot2)
+                    Destroy(robot2);
+                robot1 = GameObject.Instantiate<GameObject>(robotPrefab1);
                 switched = true;
                 break;
-            case true:   
-                Instantiate(robot1, transform.position, transform.rotation);
+            case true:
+                if(robot1)
+                    Destroy(robot1);
+                robot2 = GameObject.Instantiate<GameObject>(robotPrefab2);   
                 switched = false;
                 break;
         }
